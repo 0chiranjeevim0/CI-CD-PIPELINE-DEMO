781c4de176fb76b9e54ea919e0c2afc3a6a366d0
CHIRANJEEVI <chiranjeevi2004@gmail.com>
2023-04-07 11:36:08 +0530

    status:CI Pipeline

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const getData = async(callback) =>{
 
 const printData = (data) =>{
     for(let i = 0;i<5;i++){
-        const todo = document.createElement('h2');
+        const todo = document.createElement('h3');
         todo.innerText = data[i].title;
         document.body.appendChild(todo);
 
